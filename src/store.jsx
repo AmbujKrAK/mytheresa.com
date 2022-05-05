43539c5b33f485e516d6684278e57df126e44df8
ankush4gupta <87088148+ankush4gupta@users.noreply.github.com>
2022-05-05 20:11:14 +0530

    added gitfile

--- src/store.jsx
+++
@@ -1,8 +0,0 @@
-import { legacy_createStore } from "redux";
-import { reducer } from "./Redux/Auth/reducer";
-
-export const store = legacy_createStore(reducer);
-
-store.subscribe(() => {
-   console.log("Subscriber:", store.getState());
-});
