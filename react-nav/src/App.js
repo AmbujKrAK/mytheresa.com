33830146a7e2eb0969cf64f70ae02f056cebeac3
Abhijeet Chaudhari <abhijeetchaudhari615@gmail.com>
2022-05-06 17:26:48 +0530

    update

--- react-nav/src/App.js
+++ react-nav/src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Heads from"./components/webview/header"
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Heads from "../../mytheresa-project/src/components/webview/header";
 
 function App() {
   return (
     <div className="App">
-       <Heads />
-     <Navbar/>
-     <hr />
+      <Heads />
+      <Navbar />
+      <hr />
     </div>
   );
 }
