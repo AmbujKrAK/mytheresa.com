5f0c0557e410597bf0ed376383c22bd709a305ed
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-05-07 13:57:41 +0530

    Thank you page done

---
+++ thankyou-page/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ThankYou from './components/ThankyouPage/thankyou';
+
+function App() {
+  return (
+    <div className="App">
+      <ThankYou/>
+    </div>
+  );
+}
+
+export default App;
