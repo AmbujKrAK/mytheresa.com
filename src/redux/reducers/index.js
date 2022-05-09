5c50a4ae4e624bab42fc9f479cd38f9f45491c3f
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-05-09 17:52:35 +0530

    added screenshots

--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { productsReducer, selectedProductsReducer } from "./productsReducer";
-import { Authreducer } from "../Auth/reducer"
-const reducers = combineReducers({
-  allProducts: productsReducer,
-  product: selectedProductsReducer,
-  Authreducer: Authreducer
-});
-export default reducers;
